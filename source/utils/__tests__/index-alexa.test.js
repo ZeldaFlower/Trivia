70119a8c799f860a973879ab417a6d424d188b04
Christine <zelda_flower@yahoo.com>
2021-07-25 19:44:38 -0500

    fix syntax error

--- source/utils/__tests__/index-alexa.test.js
+++ source/utils/__tests__/index-alexa.test.js
@@ -66,6 +66,7 @@ alexaTest.test([
 		says: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
 		reprompts: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses. Please say one, two, three, or four.", 
 		shouldEndSession: false
+	}
 ]);
 });
 
