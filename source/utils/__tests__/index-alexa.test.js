22bbd080963f3d5681aaaf32e5f098b31e02414c
Christine <zelda_flower@yahoo.com>
2021-07-25 19:38:56 -0500

    Add GetTriviaQuestion test

--- source/utils/__tests__/index-alexa.test.js
+++ source/utils/__tests__/index-alexa.test.js
@@ -57,6 +57,14 @@ alexaTest.test([
 		}
 	}
 ]);
+	
+alexaTest.test([
+	{
+		request: alexaTest.getIntentRequest("GetTriviaQuestion", [categoryTitle: "animal"]),
+		says: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
+		reprompts: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses. Please say one, two, three, or four.", 
+		shouldEndSession: false
+]);
 });
 
 // 	describe("GetTriviaQuestion", function () {
