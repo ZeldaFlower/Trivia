7505cce4b2bdb074defec3837c9d73ce6e4165b5
Christine <zelda_flower@yahoo.com>
2021-07-25 19:42:37 -0500

    update slots

--- source/utils/__tests__/index-alexa.test.js
+++ source/utils/__tests__/index-alexa.test.js
@@ -60,7 +60,9 @@ alexaTest.test([
 	
 alexaTest.test([
 	{
-		request: alexaTest.getIntentRequest("GetTriviaQuestion", [categoryTitle: "animal"]),
+		request: alexaTest.getIntentRequest("GetTriviaQuestion", "categoryTitle": {
+					"name": "categoryTitle",
+					"value": "Animal"}),
 		says: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
 		reprompts: "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses. Please say one, two, three, or four.", 
 		shouldEndSession: false
