6efa8f635a1ea73c23425e8b08ed99aca1fd1eaf
Christine <zelda_flower@yahoo.com>
2021-10-03 11:58:58 -0500

    Update index-alexa.test.js

--- source/utils/__tests__/index-alexa.test.js
+++ source/utils/__tests__/index-alexa.test.js
@@ -55,12 +55,12 @@ describe("Trivia Skill", function () {
 		    secretAccessKey: "bogussecretkey"
 		});
 
-		aws.DynamoDB.DocumentClient.prototype.get.mockImplementation((params, cb) => {// Oops, had _ here nd params variable uncommented
+		aws.DynamoDB.DocumentClient.prototype.get.mockImplementation((params, cb) => {
 		  cb(null, { "Item": {
 			  "question": "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
 			  "category": "Animal",
 			  "answerNumber": "3",
-			  "triviaId": "3"
+			  "triviaID": "3"
 		  }});
 		});
 		aws.DynamoDB.DocumentClient.prototype.put.mockImplementation((_, cb) => {
