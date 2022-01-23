68874c0fe8f6eca7d81d41f88c1208d839601efb
Christine <zelda_flower@yahoo.com>
2022-01-23 12:27:50 -0600

    Fix syntax

--- source/utils/__tests__/index-alexa.test.js
+++ source/utils/__tests__/index-alexa.test.js
@@ -50,16 +50,17 @@ describe("Trivia Skill", function () {
 
 		aws.DynamoDB.DocumentClient.prototype.get.mockImplementation((params, cb) => {
 			if (params.Key.triviaID == "animal") {
-		  cb(null, { "Item": {
-			  "questionKeys": "3"
-		  }});
+				cb(null, { "Item": {
+					"questionKeys": "3"
+				}});
 			} else {
 				cb(null, { "Item": {
-			  "question": "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
-			  "category": "Animal",
-			  "answerNumber": "3",
-			  "triviaID": "3"
-		  }});
+					"question": "What is Christine's favorite animal? 1) Cats 2) Dogs 3) Bunnies 4) Horses.", 
+					"category": "Animal",
+					"answerNumber": "3",
+					"triviaID": "3"
+				}});
+			}
 		});
 		aws.DynamoDB.DocumentClient.prototype.put.mockImplementation((_, cb) => {
 		  cb(null, null);
