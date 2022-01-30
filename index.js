910a1a1d1a23812a8930212e3252319c4f8d9889
Christine <zelda_flower@yahoo.com>
2022-01-30 12:30:46 -0600

    save question after figuring out what question number it should be

--- index.js
+++ index.js
@@ -1837,7 +1837,7 @@ function emailVerses(email, date) {
 // Alexa: what is the correct answer?
 // 2
 function addQuestionForUser(filledSlots, userId) {
-		if (filledSlots != undefined){
+		if (filledSlots != undefined) {
 console.log("adding question")
 console.log(filledSlots)
 	const question = filledSlots.slots.question.value; //  Out of the following, what is a tomato? 
@@ -1883,86 +1883,87 @@ console.log(filledSlots)
 			// increment and add to list, and save
 			dbPut({TableName: "trivia", Item: item.Item}).then(function(){
 					// successful
+					
+	// TODO: then save trivia too
+	var dynamoParams = {
+		TableName: "trivia",
+		Item: {
+		  triviaID: userId+category.toLowerCase()+questionNumber,
+		  category: category,
+		  question: finalQuestion,
+		  answerNumber: answer,
+		  answer: answersList[answer-1],
+		  updated: Date.now()
+		}
+	  };
+	  console.log("!!!");
+	  console.log(dynamoParams);		
+	  checkIfUserExists.call(this, userId)
+		.then(function (existingData) {
+		  var existingItem = existingData.Item;
+  // 		if (existingItem) {
+  // 			existingItem.userID = userId;
+  // 			dynamoParams.Item = existingItem;
+  // 		}
+  // 		var nameList = name.split(" and ");
+  // 		var matchingFood = getTriviaQuestion.call(this, existingItem, nameList, location, granularLocation);
+  // // 		var alreadyExists = "You already have ";
+  // 		var alreadyExisting = {}
+  // 		for (let food of matchingFood) {
+  // 			alreadyExisting[food.name] = true;
+  // 			alreadyExists = alreadyExists +food.name+" in your "+food.location+" "+(food.granularLocation||"")+", ";
+  // 		}
+  // 		var names = [];
+  // 		for (let name of nameList) {
+  
+  // 			let ignore = alreadyExisting[name];
+  // 			if (!ignore) {
+  // 				names.push(name);
+  // 				var newFood = {
+  // 					name: name,
+  // 					location: location,
+  // 					granularLocation: granularLocation,
+  // 					// condiment: condiment,
+  // 					dateAdded: Date.now(),
+  // 					useByDate: useByDate
+  // 				};
+  // 				dynamoParams.Item.food.push(newFood);
+  // 				dynamoParams.Item.food.sort((a,b) => {
+  //   					if (('' + a.location).localeCompare(b.location)>0){
+  // 						return 1;
+  // 					}
+  //                     if (('' + a.location).localeCompare(b.location)==0){
+  //                     	if(('' + a.granularLocation).localeCompare(b.granularLocation)>0){
+  //                         	return 1;
+  //                         }
+  //                         if (('' + a.granularLocation).localeCompare(b.granularLocation) == 0) {
+  //                         	if(('' + a.food).localeCompare(b.food)>0){
+  //                             	return 1;
+  //                             } else {
+  //                             	return 0;
+  //                             }
+  //                         }
+  //                         return -1
+  // 					}
+  //                     return -1
+  // 				});
+  
+  // 				dynamoParams.Item.updated = Date.now();
+  // 			}
+  
+  // 		}
+		  // tell which you already have, and what added, number added
+		  putParamsAndMessage.call(this, dynamoParams, "Added your question '"+question+"' with category "+category+", answers: "+answers+" and correct answer "+correctAnswer+".", ":tellWithCard", this.t('TRIVIA_INFO_TITLE'));
+  
+	  }.bind(this)).catch(err => {
+		  console.error(err);
+		  this.emit(':ask', this.t('SORRY'));
+	  });
 			}.bind(this)).catch(err => {
 				console.error(err);
 				this.emit(':ask', this.t('SORRY'));
 			});
 	})
-	// TODO: then save trivia too
-	var dynamoParams = {
-	  TableName: "trivia",
-	  Item: {
-		triviaID: userId+category.toLowerCase()+questionNumber,
-		category: category,
-		question: finalQuestion,
-		answerNumber: answer,
-		answer: answersList[answer-1],
-		updated: Date.now()
-	  }
-	};
-	console.log("!!!");
-	console.log(dynamoParams);		
-	checkIfUserExists.call(this, userId)
-	  .then(function (existingData) {
-		var existingItem = existingData.Item;
-// 		if (existingItem) {
-// 			existingItem.userID = userId;
-// 			dynamoParams.Item = existingItem;
-// 		}
-// 		var nameList = name.split(" and ");
-// 		var matchingFood = getTriviaQuestion.call(this, existingItem, nameList, location, granularLocation);
-// // 		var alreadyExists = "You already have ";
-// 		var alreadyExisting = {}
-// 		for (let food of matchingFood) {
-// 			alreadyExisting[food.name] = true;
-// 			alreadyExists = alreadyExists +food.name+" in your "+food.location+" "+(food.granularLocation||"")+", ";
-// 		}
-// 		var names = [];
-// 		for (let name of nameList) {
-
-// 			let ignore = alreadyExisting[name];
-// 			if (!ignore) {
-// 				names.push(name);
-// 				var newFood = {
-// 					name: name,
-// 					location: location,
-// 					granularLocation: granularLocation,
-// 					// condiment: condiment,
-// 					dateAdded: Date.now(),
-// 					useByDate: useByDate
-// 				};
-// 				dynamoParams.Item.food.push(newFood);
-// 				dynamoParams.Item.food.sort((a,b) => {
-//   					if (('' + a.location).localeCompare(b.location)>0){
-// 						return 1;
-// 					}
-//                     if (('' + a.location).localeCompare(b.location)==0){
-//                     	if(('' + a.granularLocation).localeCompare(b.granularLocation)>0){
-//                         	return 1;
-//                         }
-//                         if (('' + a.granularLocation).localeCompare(b.granularLocation) == 0) {
-//                         	if(('' + a.food).localeCompare(b.food)>0){
-//                             	return 1;
-//                             } else {
-//                             	return 0;
-//                             }
-//                         }
-//                         return -1
-// 					}
-//                     return -1
-// 				});
-
-// 				dynamoParams.Item.updated = Date.now();
-// 			}
-
-// 		}
-		// tell which you already have, and what added, number added
-		putParamsAndMessage.call(this, dynamoParams, "Added your question '"+question+"' with category "+category+", answers: "+answers+" and correct answer "+correctAnswer+".", ":tellWithCard", this.t('TRIVIA_INFO_TITLE'));
-
-	}.bind(this)).catch(err => {
-		console.error(err);
-		this.emit(':ask', this.t('SORRY'));
-	});
 		}
 }
 
