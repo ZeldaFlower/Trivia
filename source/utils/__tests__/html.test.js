cb14237d8fa4ea836463cc8b035fbb14889d1344
Christine <zelda_flower@yahoo.com>
2023-09-19 13:24:09 -0500

    fix syntax

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -18,13 +18,13 @@ describe("html", function () {
 		var searchString = "Automation testing with Selenium";
 		
 		//To wait for browser to build and launch properly
-		let driver = await new Builder().forBrowser("chrome").build();
+		let driver = await new webdriver.Builder().forBrowser("chrome").build();
 		
 		//To fetch http://google.com from the browser with our code.
 		await driver.get("https://s3.amazonaws.com/christine-trivia/index.html");
 		    
 		//To send a search query by passing the value in searchString.
-		await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
+		await driver.findElement(webdriver.By.name("q")).sendKeys(searchString,Key.RETURN);
 		
 		//Verify the page title and print it
 		var title = await driver.getTitle();
