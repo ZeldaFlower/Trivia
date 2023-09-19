c9ea3e208941eb56456597992aec3c22beb1b5a1
Christine <zelda_flower@yahoo.com>
2023-09-19 13:01:54 -0500

    fix html test

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -15,21 +15,24 @@ describe("html", function () {
 	});
 	
 	describe("test html", function () {
-    var browser_name = new webdriver.Builder();
-    
-    withCapabilities(webdriver.Capabilities.firefox()).build();
-    
-    browser_name.get('https://s3.amazonaws.com/christine-trivia/index.html');
-    
-    var promise = browser_name.getTitle();
-    
-    promise.then(function(title) {
-    
-      console.log(title);
-    
-    });
-    
-    browser.quit();
+		var searchString = "Automation testing with Selenium";
+		
+		//To wait for browser to build and launch properly
+		let driver = await new Builder().forBrowser("chrome").build();
+		
+		//To fetch http://google.com from the browser with our code.
+		await driver.get("https://s3.amazonaws.com/christine-trivia/index.html");
+		    
+		//To send a search query by passing the value in searchString.
+		await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
+		
+		//Verify the page title and print it
+		var title = await driver.getTitle();
+		console.log('Title is:',title);
+		
+		//It is always a safe practice to quit the browser after execution
+		await driver.quit();
+
 	});
 
 });
