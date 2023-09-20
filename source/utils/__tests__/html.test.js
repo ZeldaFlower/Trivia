22e1b7e55660293d86e00343434b57c36a71b725
Christine <zelda_flower@yahoo.com>
2023-09-20 10:05:58 -0500

    try chrome options

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -6,6 +6,8 @@ Run with 'mocha examples/skill-sample-nodejs-hello-world/helloworld-tests.js'.
 
 // include the testing framework
 var webdriver = require('selenium-webdriver');
+// from selenium import webdriver
+// from selenium.webdriver.chrome.options import Options
 
 describe("html", function () {
 	
@@ -17,18 +19,25 @@ describe("html", function () {
 	test("test html", async function () {
 		var searchString = "Automation testing with Selenium";
 		
+		var chrome_options = webdriver.chrome.Options()
+		chrome_options.add_argument('--headless')
+		chrome_options.add_argument('--no-sandbox')
+		chrome_options.add_argument('--disable-dev-shm-usage')
+		var driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+		driver.get('https://s3.amazonaws.com/christine-trivia/index.html')
+		
 		//To wait for browser to build and launch properly
-		let driver = await new webdriver.Builder().forBrowser("chrome").build();
+		// let driver = await new webdriver.Builder().forBrowser("chrome").build();
 		
 		//To fetch http://google.com from the browser with our code.
-		await driver.get("https://s3.amazonaws.com/christine-trivia/index.html");
+		// await driver.get("https://s3.amazonaws.com/christine-trivia/index.html");
 		    
 		//To send a search query by passing the value in searchString.
 		//await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
 		
 		//Verify the page title and print it
 		var title = await driver.getTitle();
-		console.log('Title is:',title);
+		console.log('Title is:', title);
 		
 		//It is always a safe practice to quit the browser after execution
 		await driver.quit();
