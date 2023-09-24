79d12308050297bcae2ef06c467fffff07098a23
Christine <zelda_flower@yahoo.com>
2023-09-24 16:27:48 -0500

    remove commented out code and add title 'assert'

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -1,17 +1,6 @@
-/*
-Mocha tests for the Alexa skill "Hello World" example (https://github.com/alexa/skill-sample-nodejs-hello-world).
-Using the Alexa Skill Test Framework (https://github.com/BrianMacIntosh/alexa-skill-test-framework).
-Run with 'mocha examples/skill-sample-nodejs-hello-world/helloworld-tests.js'.
-*/
-
-// // include the testing framework
-// var webdriver = require('selenium-webdriver');
-// require('jest-environment-jsdom');
 const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const assert = require('node:assert');
-// from selenium import webdriver
-// from selenium.webdriver.chrome.options import Options
 
 jest.setTimeout(50000)
 
@@ -23,26 +12,18 @@ describe("html", function () {
 	});
 	
 	test("test html", async function () {
-		// example from selenium website:
 		console.log(process.env)
 		var builder = new Builder().forBrowser('firefox');
-		// var capabilities = Capabilities.firefox();
-		// var options = {
-		//     'args':  [ "--headless", "--no-sandbox", "--window-size=1420,1080", "--disable-gpu", "--disable-dev-shm-usage" ]
-		// };
-
-		// capabilities.set('firefoxOptions', options)
-		// capabilities['loggingPrefs'] = {'browser': 'ALL'}
-		// builder.withCapabilities(capabilities);
 		let options = new firefox.Options();
 		builder.setFirefoxOptions(options.addArguments('--headless'));
 		let driver
 		try {
 			driver = await builder.build();
 			await driver.get('https://s3.amazonaws.com/christine-trivia/index.html');
-			var title = await driver.findElement(By.xpath('body > div:nth-child(1) > h2')).getAttribute('innerHTML').text;
-			console.log(title)
-			assert.equal(title, "Christine Trivia");
+			await driver.wait(until.titleIs('s3.amazonaws.com/christine-trivia/index.html'), 1000);
+			// var title = await driver.findElement(By.xpath('body > div:nth-child(1) > h2')).getAttribute('innerHTML').text;
+			// console.log(title)
+			// assert.equal(title, "Christine Trivia");
 			// await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
 			// await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 			// driver.get('https://s3.amazonaws.com/christine-trivia/index.html');
@@ -60,48 +41,6 @@ describe("html", function () {
 				await driver.quit();
 			}
 		}
-		
-		// var chromeCapabilities = webdriver.Capabilities.chrome();
-		// //setting chrome options to start the browser fully maximized
-		// var chromeOptions = {
-		//     'args': [ '--start-maximized', "--headless", "--no-sandbox", "--window-size=1420,1080", "--disable-gpu", "--disable-dev-shm-usage", "disable-extensions", "--disable-infobars", "--remote-debugging-port=9222" ]
-		// };
-		// chromeCapabilities.set('chromeOptions', chromeOptions);
-		// var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
-
-
-		
-		// var searchString = "Automation testing with Selenium";
-		
-		// chrome_options = webdriver.ChromeOptions()
-		// chrome_options.add_argument('--no-sandbox')
-		// chrome_options.add_argument('--window-size=1420,1080')
-		// chrome_options.add_argument('--headless')
-		// chrome_options.add_argument('--disable-gpu')
-		// driver = webdriver.Chrome(chrome_options=chrome_options)
-		
-		// var chrome_options = webdriver.chrome.Options()
-		// chrome_options.add_argument('--headless')
-		// chrome_options.add_argument('--no-sandbox')
-		// chrome_options.add_argument('--disable-dev-shm-usage')
-		// var driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
-		// driver.get('https://s3.amazonaws.com/christine-trivia/index.html')
-		
-		// //To wait for browser to build and launch properly
-		// // let driver = await new webdriver.Builder().forBrowser("chrome").build();
-		
-		// //To fetch http://google.com from the browser with our code.
-		// // await driver.get("https://s3.amazonaws.com/christine-trivia/index.html");
-		    
-		// //To send a search query by passing the value in searchString.
-		// //await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
-		
-		// //Verify the page title and print it
-		// var title = await driver.getTitle();
-		// console.log('Title is:', title);
-		
-		// //It is always a safe practice to quit the browser after execution
-		// await driver.quit();
 
 	});
 
