b3708567e71dc1aa6bf8ee248bd578d01c454795
Christine <zelda_flower@yahoo.com>
2023-10-26 20:41:22 -0500

    print body

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -24,6 +24,8 @@ describe("html", function () {
 			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="Search"]'))), 1000).sendKeys('webdriver', 'carrots');
 			await driver.findElement(By.xpath('//*[@id="Search"]')).sendKeys('webdriver', Key.RETURN);
 
+			
+			console.log(document.body)
 			await driver.wait(until.elementIsVisible(driver.findElement(By.className("sc-pc-medium-desktop-card-canary-product-link"))), 1000).click();
 			
 			// var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[1]'))), 1000);
