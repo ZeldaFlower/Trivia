dc1ca158a5f87c9d87573539140a02ce20e0e351
Christine <zelda_flower@yahoo.com>
2023-11-12 16:57:30 -0600

    try different class

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -25,7 +25,7 @@ describe("html", function () {
 			// await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="Search"]'))), 1000)
 			await driver.wait(until.titleIs("Search for carrots - Sam's Club"), 5000);
 			
-			await driver.wait(until.elementIsVisible(driver.findElement(By.css(".sc-pc-medium-desktop-card-canary-product-link"))), 5000).click();
+			await driver.wait(until.elementIsVisible(driver.findElement(By.css(".sc-plp-cards-card-first-row-item"))), 5000).click();
 			
 			// var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[1]'))), 1000);
 			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="page-title"]'))), 1000);
