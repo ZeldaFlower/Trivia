45a1ea903e51d80ee4187dd24982a467922825e6
Christine <zelda_flower@yahoo.com>
2023-11-12 18:05:53 -0600

    wait for broccoli florets tab title

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -37,7 +37,7 @@ describe("html", function () {
 			// await driver.findElement(By.css('bst-btn-secondary')).click();
 
 			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="Search"]'))), 1000).sendKeys('broccoli florets', Key.RETURN);
-			await driver.wait(until.titleIs("Sam's Club"), 5000);
+			await driver.wait(until.titleIs("Search for broccoli florets - Sam's Club"), 5000);
 			await driver.wait(until.elementIsVisible(driver.findElement(By.css(".sc-pc-title-medium"))), 5000).click();
 
 			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[1]/h1'))), 5000);
