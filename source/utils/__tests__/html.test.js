797f2b91137ac7854cea186872b9a2f653707128
Christine <zelda_flower@yahoo.com>
2023-11-12 18:25:22 -0600

    add logging

--- source/utils/__tests__/html.test.js
+++ source/utils/__tests__/html.test.js
@@ -36,13 +36,16 @@ describe("html", function () {
 			// await driver.findElement(By.css('bst-btn')).click();
 			// await driver.findElement(By.css('bst-btn-secondary')).click();
 
-			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="Search"]'))), 1000).sendKeys('broccoli florets', Key.RETURN);
+			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="Search"]'))), 5000).sendKeys('broccoli florets', Key.RETURN);
 			await driver.wait(until.titleIs("Search for broccoli florets - Sam's Club"), 5000);
-			await driver.wait(until.elementIsVisible(driver.findElement(By.css(".sc-pc-title-medium"))), 5000).click();
-
+			var element = await driver.wait(until.elementIsVisible(driver.findElement(By.css(".sc-pc-title-medium"))), 5000)
+			console.log("I'm here");
+			console.log(element) 
+			element.click();
 			var title = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[2]/h1'))), 5000);
 			console.log(title)
-			await driver.wait(until.elementIsVisible(driver.findElement(By.css('bst-btn'))), 5000);////*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[2]/h1
+			await driver.wait(until.elementIsVisible(driver.findElement(By.css('bst-btn'))), 5000);////*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[2]/h1e
+			//*[@id="main"]/div/div/div[1]/div/div[3]/div[2]/div[2]/h1
 			
 			await driver.findElement(By.css('bst-btn')).click();
 			await driver.findElement(By.css('bst-btn-secondary')).click(); // continue shopping
